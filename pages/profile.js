f82bf3af484da9c336958cc4b182cecb4afa1f15
José Luiz Coe <joseluizcoe@gmail.com>
2017-11-07 23:03:03 -0200

    Using Default layout in profile page... just works

---
+++ pages/profile.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import Layout from '../layouts/default';
+
+
+class Profile extends Component {
+  render() {
+
+    return (
+      <Layout>
+        <h1>Profile Page</h1>
+      </Layout>
+    )
+
+  }
+}
+
+export default Profile;
