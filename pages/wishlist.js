1a304ca80d05f5f51e608feeaa2870212ea21284
José Luiz Coe <joseluizcoe@gmail.com>
2017-11-08 01:54:00 -0200

    Renaming wishlist page fine

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -1,11 +1,14 @@
 import { Component } from 'react';
 import Layout from '../layouts/default';
+import { Segment, Icon } from 'semantic-ui-react';
 
 class Profile extends Component {
   render() {
     return (
       <Layout>
-        <h1>Wishlist Page</h1>
+        <h1>
+          <Icon name='heart' /> Wishlist
+        </h1>
       </Layout>
     )
   }
