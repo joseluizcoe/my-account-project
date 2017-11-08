17fbd61c24eee7a4468d373960196f42a6e0bde0
José Luiz Coe <joseluizcoe@gmail.com>
2017-11-08 01:53:11 -0200

    Adding Top and submenu to layout default

--- layouts/default.js
+++ layouts/default.js
@@ -1,26 +1,29 @@
-import { Component } from 'react';
-import {
-  Container,
-  Grid,
-} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import { Top, Submenu } from '../components';
 
-export default class LayoutDefault extends Component {
+class LayoutDefault extends Component {
   constructor({ children, actions, nav }) {
     super();
     this.children = children;
   }
-
   render() {
     return (
-      <div>
+      <div style={{ marginTop: '4em' }}>
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width'
+        />
         <link
           rel="stylesheet"
           href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"
         />
-        <Container fluid>
-          {this.children}
-        </Container>
+        <Top />
+        <Submenu />
+        {this.children}
       </div>
     )
   }
 }
+
+export default LayoutDefault
