bb7b48130862158b075d28878728737cdb6bb6a1
José Luiz Coe <joseluizcoe@gmail.com>
2017-11-09 00:27:05 -0200

    Getting order history from data file (if i have time, will be database)

--- pages/order-history.js
+++ pages/order-history.js
@@ -1,18 +1,38 @@
 import { Component } from 'react';
 import Layout from '../layouts/default';
-import { Icon } from 'semantic-ui-react';
+import { Button, Image, Icon, Item, Label, Header, Divider } from 'semantic-ui-react';
+import data from '../data/order-history';
 
-class Profile extends Component {
+
+class OrderHistory extends Component {
   render() {
-    return (
+    console.log(data);
+    return(
       <Layout>
-        <h1>
-          <Icon name='shopping basket' /> Order history
-        </h1>
-
+        <Header as='h5'>
+          <Icon name='shopping basket' /> Order History
+        </Header>
+        <Divider />
+        {data.orders.map(
+          order =>
+            <Item.Group unstackable divided key={order.id}>
+              { order.products.map( (product)=>
+                <Item key={product.sku}>
+                  <Item.Image floated='left' size='tiny' src={product.image} />
+                  <Item.Content>
+                    <Item.Header>{product.title}</Item.Header>
+                    <Item.Meta>
+                    <Button color='teal'>Details</Button>
+                    </Item.Meta>
+                  </Item.Content>
+                </Item>
+              )}
+          </Item.Group>
+        )
+        }
       </Layout>
     )
   }
 }
 
-export default Profile;
+export default OrderHistory;
