816188fb104c1bf22c380a8350791f7d392e2059
José Luiz Coe <joseluizcoe@gmail.com>
2017-11-10 03:06:25 -0200

    Connectin Wishlist Page to store.

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -1,9 +1,22 @@
 import { Component } from 'react';
+import withRedux from 'next-redux-wrapper';
+import store from '../redux/store';
+
 import Layout from '../layouts/default';
 import { Segment, Icon, Item, Button } from 'semantic-ui-react';
 import data from '../data/wishlist';
 
 class Wishlist extends Component {
+  handleAddToCart = () => {
+    const { dispatch, cartItems } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      cartItems,
+    });
+    return false;
+  }
+
   render() {
     return (
       <Layout iconName='heart' title='Wishlist'>
@@ -15,7 +28,9 @@ class Wishlist extends Component {
                 <Item.Header>{product.title}</Item.Header>
                 <Item.Meta>R$ {product.price}</Item.Meta>
                 <Item.Meta>
-                  <Button color='teal'><Icon name='cart' size='large'/> Add to cart</Button>
+                  <Button color='teal' onClick={this.handleAddToCart}>
+                    <Icon name='cart' size='large'/> Add to cart
+                  </Button>
                 </Item.Meta>
               </Item.Content>
             </Item>
@@ -26,4 +41,13 @@ class Wishlist extends Component {
   }
 }
 
-export default Wishlist;
+const mapStateToProps = (state) => {
+  const { loading, activeLink, cartItems }  = state;
+  return {
+    loading,
+    activeLink,
+    cartItems
+  };
+}
+
+export default withRedux(store, mapStateToProps)(Wishlist);
